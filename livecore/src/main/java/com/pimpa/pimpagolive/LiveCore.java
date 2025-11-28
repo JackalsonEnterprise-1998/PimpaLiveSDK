679e7df05134fe5ae9db5631c2421ceceff8afbf
JackalsonEnterprise-1998 <jackalsonadmob46664@gmail.com>
2025-11-28 02:05:10 -0800

    Create LiveCore.java

---
+++ livecore/src/main/java/com/pimpa/pimpagolive/LiveCore.java
@@ -0,0 +1,11 @@
+package com.pimpa.pimpagolive;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class LiveCore {
+
+    public static void initialize(Context context) {
+        Toast.makeText(context, "Pimpa Live SDK Initialized", Toast.LENGTH_SHORT).show();
+    }
+}
